a00e858d641234af51aa849bd30ebc569a93974c
Sherif El-Aassar <sherifelaassar@outlook.com>
2021-06-14 13:16:49 +0200

    Initial commit

---
+++ DiabeticRetinopathyDetection/urls.py
@@ -0,0 +1,30 @@
+"""DiabeticRetinopathyDetection URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import myapp
+    2. Add a URL to urlpatterns:  path('', myapp.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+#from myapp import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('myapp.urls')),
+    #url(r'^$', views.home_page, name='myapp-Page')
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# DiabeticRetinopathyDetection
+Login, Register, Upload image
+127.0.0.1:8000 ---> Home page (no user)
+127.0.0.1:8000/sherif/home ---> Home page (user logged in)
+127.0.0.1:8000/sherif/profile ---> User's profile
+127.0.0.1:8000/sherif/result ---> Result of sent image by a logged in user (Not made yet !!)
